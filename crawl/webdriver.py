9a57e3a108f3ad4cdbc558c2a10502f2a744ae43
yumdari <epqmfzkdlwj@naver.com>
2024-04-19 19:54:55 +0900

    selenium find elements

--- crawl/webdriver.py
+++ crawl/webdriver.py
@@ -1,26 +1,15 @@
 
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 # 웹드라이버 설정 (ex. Chrome)
-driver = webdriver.Chrome('C:/Users/hun/Downloads/chromedriver-win64chromedriver/chromedriver.exe')
+driver = webdriver.Chrome()
 
 driver.get('https://www.naver.com/')
 
 # h1 태그 모두 찾기
-h1_tags = driver.find_elements_by_tag_name('div')
+h1_tags = driver.find_elements(By.TAG_NAME,'div')
 for tag in h1_tags:
     print(tag.text)
 
 driver.close()
-
-'''
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-
-CHROME_DRIVER_PATH = 'C:/Users/hun/Downloads/chromedriver-win64chromedriver/chromedriver.exe'
-service = Service(executable_path=CHROME_DRIVER_PATH)
-options = webdriver.ChromeOptions()
-driver = webdriver.Chrome(service=service, options=options)
-
-driver.get('https://www.naver.com/')
-'''
