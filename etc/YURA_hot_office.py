03936dc59ab4d5c4c87c618fa7f94bd0b7bbe84d
yumdari <epqmfzkdlwj@naver.com>
2024-04-19 17:04:56 +0900

    계산식 매개변수 변경 (온도+풍속 -> 온도+습도)

--- etc/YURA_hot_office.py
+++ etc/YURA_hot_office.py
@@ -3,43 +3,47 @@ YURA 6th floor perceived temperature
 
 the hottest office in the world
 
---------------------------------Parameter----------------------------------
-* 실내 섭씨 기온 temp(ºC)
+---------------------------------Formula-----------------------------------
 
-* 절대 습도 humi(%)
-1㎥ 공기 중에 포함되어 있는 수증기의 질량 (kg)
+https://data.kma.go.kr/climate/windChill/selectWindChillChart.do
 
-* 선풍기 풍속 val(km/h)
+※ 2022.6.2. 기준 여름철 체감온도 산출식 변경(2022.6.2.이전 3.5)
+
+- 체감온도 = -0.2442 + 0.55399Tw + 0.45535Ta – 0.0022Tw2 + 0.00278TwTa + 3.0(기존 3.5)
+
+* Tw = TaATAN[0.151977(RH+8.313659)1/2] + ATAN(Ta+RH) - ATAN(RH-1.67633) + 0.00391838RH3/2ATAN(0.023101RH) - 4.686035
+
+** Ta : 기온(°C), Tw : 습구온도(Stull의 추정식 이용), RH : 상대습도(%)
 ---------------------------------------------------------------------------
-
-체감온도 = 13.12 + 0.6215*(기온) - 11.37*(풍속^0.16) + 0.3965*(풍속^0.16)*(기온)
-
 """
 import math
 import os
 
-def calc(temp, val):
-    perc_temp = 13.12 + 0.6215*float(temp) - 11.37*math.pow(float(val),0.16) + 0.3965*math.pow(float(val),0.16)*float(temp)
-    return perc_temp
+def calculate_tw(Ta, RH):
+    #Ta = float(Ta)
+    #RH = float(RH)
+    Tw = Ta + 0.151977 * ((RH + 8.313659) ** 0.5) + math.atan(Ta + RH) - math.atan(RH - 1.67633) + 0.00391838 * RH ** 1.5 * math.atan(0.023101 * RH) - 4.686035
+    return Tw
+
+def calculate_expression(Tw, Ta):
+    #Ta = float(Ta)
+    expression_value = -0.2442 + 0.55399 * Tw + 0.45535 * Ta - 0.0022 * Tw ** 2 + 0.00278 * Tw * Ta + 3.0
+    return expression_value
 
 '''
 사무실 기온 입력
 '''
-temp = input("사무실 기온 입력(ºC) : ")
+temp = float(input("사무실 기온 입력(ºC) : "))
 
 '''
-선풍기 작동 여부
+사무실 습도 입력
 '''
-onOff = input("선풍기 ON? (y/n)")
-if (onOff == "y") :
-    str = input("강풍? (y/n)")
-    if (str == "y") : 
-        val = 1.11 # 탁상용 선풍기 평균 최대 풍속 4m/s -> 1.11km/h
-    else : 
-        val = 0.83 # 탁상용 선풍기 평균 최저 풍속 3m/s -> 0.83km/h
-else : 
-    val = 0
+humi = float(input("사무실 습도 입력(%) : "))
 
-print("체감 온도는 ", round(calc(temp,val),2), "ºC입니다.")
+Tw = calculate_tw(temp, humi)
+
+perv_temp = calculate_expression(Tw, temp)
+
+print("체감 온도는 ", round(perv_temp,2), "ºC입니다.")
 
 os.system("pause")
